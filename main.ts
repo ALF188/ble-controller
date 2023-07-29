e02c9801a5e48518bdb3d5532c9213314826c79a
ALF188 <adam.felder88@gmail.com>
2023-07-29 16:12:15 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 gamepad.startGamepadService()
 let y = 0
 let x = 0
+let multiple = parseFloat("8.05511811023622")
 basic.forever(function () {
     x = input.acceleration(Dimension.X)
     y = input.acceleration(Dimension.Y)
